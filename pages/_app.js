dbda9707aa5753694dda3d62ff7c8f7f6c669b8d
Arthur <arthurfraim@gmail.com>
2022-11-04 12:32:17 +0800

    successful get data and passed into cards

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
