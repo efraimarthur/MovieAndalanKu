1cdc4c158372eff282c45e6cd356cae12bb25afb
Arthur <arthurfraim@gmail.com>
2022-11-09 20:17:28 +0800

    add button to scroll up

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,13 @@ import { useEffect, useState } from "react";
 import Link from "next/link";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import { Icon } from "@iconify/react";
 
 const Home = () => {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
   const [detailCard, setDetailCard] = useState({});
+  const [scroll, setScroll] = useState(1);
 
   //to get input value onchange
   const [searchval, setSearchval] = useState();
@@ -25,6 +26,16 @@ const Home = () => {
   };
   // console.log(valSearch);
   // console.log(searchval);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      const scrollCheck = window.scrollY < 700;
+      if (scroll !== scrollCheck) {
+        setScroll(scrollCheck);
+        // console.log(scrollCheck);
+      }
+    });
+  });
 
   // Fetch data using SWR
   const fetcher = (url) => fetch(url).then((r) => r.json());
@@ -49,8 +60,9 @@ const Home = () => {
   //set the final respons to hasil (ready to loop)
   const hasil = data.Search;
 
-  const detailOnclick = (item) => {
-    console.log(item);
+  //when detail button on click, set modal to show(true), and set items attributes to detailcard
+  const detailOnClick = (item) => {
+    // console.log(item);
     const cardz = {
       id: item.imdbID,
       title: item.Title,
@@ -60,9 +72,8 @@ const Home = () => {
     };
     setShow(true);
     setDetailCard(cardz);
-
-    // console.log(detailCard, "ini detail");
   };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -70,130 +81,133 @@ const Home = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div>
+        {scroll ? (
+          console.log("masih di atas")
+        ) : (
+          <a href="#" className="fixed-bottom text-decoration-none me-2">
+            <span className="btn-up float-end mb-5 p-1 bg-dark rounded-circle">
+              <Icon icon="noto:rocket" width="50" height="50" />
+            </span>
+            {/* <span className="">Scroll to top</span> */}
+          </a>
+        )}
 
-      <main className={styles.main}>
-        <div className="row justify-content-center">
-          <div className="col-md-5">
-            <form className="d-flex" role="search">
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search the Title"
-                aria-label="Search"
-                onChange={(e) => setSearchval(e.target.value)}
-              />
-              <button
-                className="btn btn-outline-success"
-                type="submit"
-                onClick={onSearch}
-              >
-                Search
-              </button>
-            </form>
+        <main className={styles.main}>
+          <div className="row justify-content-center">
+            <div className="col-md-5">
+              <form className="d-flex" role="search">
+                <input
+                  className="form-control me-2"
+                  type="search"
+                  placeholder="Search the Title"
+                  aria-label="Search"
+                  onChange={(e) => setSearchval(e.target.value)}
+                />
+                <button
+                  className="btn btn-outline-success"
+                  type="submit"
+                  onClick={onSearch}
+                >
+                  Search
+                </button>
+              </form>
+            </div>
           </div>
-        </div>
-        <div className="row">
-          <h4 className="mt-4">
-            You searched <b className="text-danger">{`"${valSearch}"`}</b>
-          </h4>
-        </div>
 
-        <div className="row mt-3">
-          <div className="d-flex flex-wrap gap-3">
-            {!hasil ? (
-              <div className="">
-                <h4 className="mt-5"> {valSearch}</h4>
-              </div>
-            ) : (
-              hasil.map((item) => (
-                <>
-                  <div
-                    className="card shadow"
-                    style={{ width: "16rem" }}
-                    key={item.imdbID}
-                  >
-                    <a className="" onClick={() => detailOnclick(item)}>
-                      <img
-                        src={item.Poster}
-                        className="card-img-top"
-                        style={{ cursor: "pointer" }}
-                        alt="..."
-                      />
-                    </a>
-                    <div className="card-body">
-                      <h5 className="card-title">{item.Title}</h5>
-                      {/* <p className="card-text">
-                        Lorem ipsum dolor sit, amet consectetur adipisicing
-                        elit. Ex possimus ab doloribus magnam ipsa perferendis!
-                      </p> */}
-
-                      <a
-                        // href="#"
-                        className="btn btn-danger"
-                        onClick={() => detailOnclick(item)}
-                        // onClick={item.imdbID}
-                      >
-                        See Details
+          <div className="row">
+            <h4 className="mt-4">
+              You searched <b className="text-danger">{`"${valSearch}"`}</b>
+            </h4>
+          </div>
+          <div className="row mt-3">
+            <div className="d-flex flex-wrap gap-3">
+              {!hasil ? (
+                <div className="">
+                  <h4 className="mt-5"> {valSearch}</h4>
+                </div>
+              ) : (
+                hasil.map((item) => (
+                  <>
+                    <div
+                      className="card shadow"
+                      style={{ width: "16rem" }}
+                      key={item.imdbID}
+                    >
+                      <a className="gambar" onClick={() => detailOnClick(item)}>
+                        <img
+                          src={item.Poster}
+                          className="card-img-top"
+                          style={{ cursor: "pointer" }}
+                          alt="..."
+                        />
                       </a>
+                      <div className="card-body">
+                        <h5 className="card-title">{item.Title}</h5>
+                        <a
+                          // href="#"
+                          className="btn btn-danger"
+                          onClick={() => detailOnClick(item)}
+                        >
+                          See Details
+                        </a>
 
-                      <Modal show={show} size="lg" onHide={handleClose}>
-                        <Modal.Header closeButton>
-                          <Modal.Title>
-                            Title :{" "}
-                            <span className="fw-bold text-danger">
-                              {detailCard.title}
-                            </span>
-                          </Modal.Title>
-                        </Modal.Header>
-                        <Modal.Body>
-                          <div className="d-flex">
-                            <img
-                              src={detailCard.image}
-                              width="200px"
-                              height="300px"
-                              alt="gmbar"
-                              className=""
-                            />
-                            <div className="ms-4 border border-end-0 border-top-0 border-bottom-0 border-4 border-danger ps-4">
-                              <p className="">
-                                <b className="fw-bold">Title : </b>
+                        <Modal show={show} size="lg" onHide={handleClose}>
+                          <Modal.Header closeButton>
+                            <Modal.Title>
+                              Title :{" "}
+                              <span className="fw-bold text-danger">
                                 {detailCard.title}
-                              </p>
-                              <p className="">
-                                <b className="fw-bold">Published Year : </b>
-                                {detailCard.year}
-                              </p>
-                              <p className="">
-                                <b className="fw-bold">Type : </b>
-                                {detailCard.type}
-                              </p>
+                              </span>
+                            </Modal.Title>
+                          </Modal.Header>
+                          <Modal.Body>
+                            <div className="d-flex">
+                              <img
+                                src={detailCard.image}
+                                width="200px"
+                                height="300px"
+                                alt="gmbar"
+                                className=""
+                              />
+                              <div className="ms-4 border border-end-0 border-top-0 border-bottom-0 border-4 border-danger ps-4">
+                                <p className="">
+                                  <b className="fw-bold">Title : </b>
+                                  {detailCard.title}
+                                </p>
+                                <p className="">
+                                  <b className="fw-bold">Published Year : </b>
+                                  {detailCard.year}
+                                </p>
+                                <p className="">
+                                  <b className="fw-bold">Type : </b>
+                                  {detailCard.type}
+                                </p>
+                              </div>
                             </div>
-                          </div>
-                        </Modal.Body>
-                        <Modal.Footer>
-                          <Button variant="danger" onClick={handleClose}>
-                            Close
-                          </Button>
-                          {/* <Button variant="primary" onClick={handleClose}>
-                            Save Changes
-                          </Button> */}
-                        </Modal.Footer>
-                      </Modal>
+                          </Modal.Body>
+                          <Modal.Footer>
+                            <Button variant="danger" onClick={handleClose}>
+                              Close
+                            </Button>
+                          </Modal.Footer>
+                        </Modal>
+                      </div>
                     </div>
-                  </div>
-                </>
-              ))
-            )}
+                  </>
+                ))
+              )}
+            </div>
           </div>
-        </div>
-      </main>
+        </main>
+      </div>
       <footer className={styles.footer}>
         <Link
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello darkness{" "}
+          Created with OMDB API{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
