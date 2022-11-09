2f318772cdccd3dc63bda7870fad9284fc23107b
Arthur <arthurfraim@gmail.com>
2022-11-09 22:13:22 +0800

    change main color to dark, added website name and fix footer

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
 import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import useSWR from "swr";
 import { useEffect, useState } from "react";
-import Link from "next/link";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import { Icon } from "@iconify/react";
@@ -77,18 +75,19 @@ const Home = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>MovieAndalanKU</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/movieIcon32.png" />
       </Head>
-      <div>
+      <div className="">
         {scroll ? (
-          console.log("masih di atas")
+          console.log("")
         ) : (
           <span className="fixed-bottom text-decoration-none me-2">
+            {/* {console.log("muncul")} */}
             <a
               href="#"
-              className="btn-up float-end mb-5 p-1 bg-dark rounded-circle"
+              className="btn-up float-end mb-5 p-1 bg-primary rounded-circle"
             >
               <Icon icon="noto:rocket" width="50" height="50" />
             </a>
@@ -98,17 +97,26 @@ const Home = () => {
 
         <main className={styles.main}>
           <div className="row justify-content-center">
+            <div className="col-md-12 py-3 text-start text-danger d-flex flex-column">
+              <p className="fw-bold fs-3">
+                Movie<span className="fw-normal text-white">Andalan</span>KU
+              </p>
+            </div>
+            <div className="col-md-12 py-3 text-center text-danger mb-2">
+              <h3 className="fw-bold">Find Movies</h3>
+            </div>
             <div className="col-md-5">
               <form className="d-flex" role="search">
                 <input
                   className="form-control me-2"
+                  // className="bg-dark"
                   type="search"
                   placeholder="Search the Title"
                   aria-label="Search"
                   onChange={(e) => setSearchval(e.target.value)}
                 />
                 <button
-                  className="btn btn-outline-success"
+                  className="btn btn-danger"
                   type="submit"
                   onClick={onSearch}
                 >
@@ -119,12 +127,12 @@ const Home = () => {
           </div>
 
           <div className="row">
-            <h4 className="mt-4">
+            <h4 className="mt-4 text-white">
               You searched <b className="text-danger">{`"${valSearch}"`}</b>
             </h4>
           </div>
           <div className="row mt-3">
-            <div className="d-flex flex-wrap gap-3">
+            <div className="d-flex flex-wrap justify-content-center justify-content-md-start gap-3">
               {!hasil ? (
                 <div className="">
                   <h4 className="mt-5"> {valSearch}</h4>
@@ -133,7 +141,7 @@ const Home = () => {
                 hasil.map((item) => (
                   <>
                     <div
-                      className="card shadow"
+                      className="card shadow bg-dark"
                       style={{ width: "16rem" }}
                       key={item.imdbID}
                     >
@@ -145,8 +153,8 @@ const Home = () => {
                           alt="..."
                         />
                       </a>
-                      <div className="card-body">
-                        <h5 className="card-title">{item.Title}</h5>
+                      <div className="card-body bg-dark">
+                        <h5 className="card-title text-white">{item.Title}</h5>
                         <a
                           // href="#"
                           className="btn btn-danger"
@@ -155,8 +163,17 @@ const Home = () => {
                           See Details
                         </a>
 
-                        <Modal show={show} size="lg" onHide={handleClose}>
-                          <Modal.Header closeButton>
+                        <Modal
+                          show={show}
+                          size="lg"
+                          centered
+                          onHide={handleClose}
+                          className="bg-whit/e"
+                        >
+                          <Modal.Header
+                            closeButton
+                            className="bg-dark text-white"
+                          >
                             <Modal.Title>
                               Title :{" "}
                               <span className="fw-bold text-danger">
@@ -164,8 +181,8 @@ const Home = () => {
                               </span>
                             </Modal.Title>
                           </Modal.Header>
-                          <Modal.Body>
-                            <div className="d-flex">
+                          <Modal.Body className="bg-dark">
+                            <div className="d-flex flex-wrap mt-sm-5 flex-md-row bg-dark">
                               <img
                                 src={detailCard.image}
                                 width="200px"
@@ -173,7 +190,7 @@ const Home = () => {
                                 alt="gmbar"
                                 className=""
                               />
-                              <div className="ms-4 border border-end-0 border-top-0 border-bottom-0 border-4 border-danger ps-4">
+                              <div className="ms-0 border border-end-0 border-top-0 border-bottom-0 border-4 border-danger ps-2 mt-md-0 mt-3 ms-md-2 text-white">
                                 <p className="">
                                   <b className="fw-bold">Title : </b>
                                   {detailCard.title}
@@ -189,7 +206,7 @@ const Home = () => {
                               </div>
                             </div>
                           </Modal.Body>
-                          <Modal.Footer>
+                          <Modal.Footer className="bg-dark">
                             <Button variant="danger" onClick={handleClose}>
                               Close
                             </Button>
@@ -204,8 +221,8 @@ const Home = () => {
           </div>
         </main>
       </div>
-      <footer className={styles.footer}>
-        <span className={styles.logo}>Created with OMDB API </span>
+      <footer className={`${styles.footer} border-top border-danger`}>
+        <span className="fw-bold text-white ">Created by Arthur Efraim</span>
       </footer>
     </div>
   );
